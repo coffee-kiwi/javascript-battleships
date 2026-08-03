3bb5a5a17dcd6a78db8248a8749859479aa0f028
meltingseaweed <conor.seo@gmail.com>
2026-08-03 11:52:31 +0900

    Tests: Rewrite initial tests. Use only 1 ship class and specify length and name upon creation

---
+++ src/shipTypes.js
@@ -0,0 +1,42 @@
+// import { Ships } from './ships.js';
+
+// export class Carrier extends Ships {
+//     constructor() {
+//         super();
+//         this.length = 5;
+//         this.numOfHits = 0;
+//         this.sunk = false;
+//     }
+// }
+
+// export class Battleship {
+//     constructor() {
+//         this.length = 4;
+//         this.numOfHits = 0
+//         this.sunk = false
+//     }
+// }
+
+// export class Cruiser {
+//     constructor() {
+//         this.length = 3;
+//         this.numOfHits = 0;
+//         this.sunk = false;
+//     }
+// }
+
+// export class Destroyer {
+//     constructor() {
+//         this.length = 3;
+//         this.numOfHits = 0;
+//         this.sunk = false;
+//     }
+// }
+
+// export class Patrol extends Ships {
+//     constructor() {
+//         this.length = 2;
+//         this.numOfHits = 0;
+//         this.sunk = false;
+//     }
+// }
---
+++ src/ships.js
@@ -0,0 +1,11 @@
+
+export class Ship {
+
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
+        this.numOfHits = 0;
+        this.sunk = false;
+    }
+
+}
