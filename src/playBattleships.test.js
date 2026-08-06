66c4f6fbef43651b1ea04af3d42952874004b8e4
meltingseaweed <conor.seo@gmail.com>
2026-08-06 11:05:36 +0900

    Remove test for unused function

--- src/playBattleships.test.js
+++ src/playBattleships.test.js
@@ -1,19 +1,20 @@
 import { Player } from './player.js'
 import { checkSetupFinished } from './playBattleships.js';
 
-describe('Checks whether the setup of ships for player is complete', () => {
-    let player1;
-    beforeEach(() => {
-        player1 = new Player("player1");
-    })
+// Remove below test as the function is no longer used. 
+// describe('Checks whether the setup of ships for player is complete', () => {
+//     let player1;
+//     beforeEach(() => {
+//         player1 = new Player("player1");
+//     })
 
-    it('Returns true when complete', () => {
-        player1.setupShips += 5;
-        expect(checkSetupFinished(player1)).toBe(true);
-    })
+//     it('Returns true when complete', () => {
+//         player1.setupShips += 5;
+//         expect(checkSetupFinished(player1)).toBe(true);
+//     })
 
-        it('Returns false if not finished', () => {
-        player1.setupShips += 4;
-        expect(checkSetupFinished(player1)).toBe(false);
-    })
-})
+//         it('Returns false if not finished', () => {
+//         player1.setupShips += 4;
+//         expect(checkSetupFinished(player1)).toBe(false);
+//     })
+// })
