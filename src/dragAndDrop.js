f2b53d39d0fbb4714f2fd4c03bf38cc71f9e8e17
meltingseaweed <conor.seo@gmail.com>
2026-08-06 12:01:09 +0900

    Refactor: Clean up index.js by moving dragAndDrop functions into it's own module. User getter and setters for variables from state. Remove the opp display as not used yet.

---
+++ src/dragAndDrop.js
@@ -0,0 +1,67 @@
+import * as listeners from './eventListeners.js';
+import { playersMessage } from './domElements.js';
+import { getCurrentPlayer, getShipName, setShipName, previewCells } from './gameState.js';
+
+export function addCellListeners(checkSetupProgress) {
+    const playercells = document.querySelectorAll('.grid-cell');
+    playercells.forEach(cell => {
+        cell.addEventListener("dragover", listeners.draggingOver);
+        
+        cell.addEventListener("dragenter", (e) => {
+            const shipName = getShipName();
+            if (!shipName) return;
+            const currentPlayer = getCurrentPlayer();
+            const myShip = currentPlayer.gameboard[shipName];
+            listeners.previewShipPlacement(e, myShip, currentPlayer.gameboard.horizontalPlacement, previewCells);
+        });
+
+        cell.addEventListener("dragleave", listeners.removePreview);
+
+        cell.addEventListener("drop", (e) => {
+            listeners.removePreview(e);
+            listeners.clearPreviewCells(previewCells);
+
+            const currentPlayer = getCurrentPlayer();
+            const shipName = getShipName();
+            let cellRow = e.target.dataset.row;
+            let cellCol = e.target.dataset.col;
+            let myShip = currentPlayer.gameboard[shipName];
+
+            try {
+                currentPlayer.gameboard.placeShip(myShip, [cellRow, cellCol]);    
+            } catch (error) {
+                playersMessage.textContent = error.message;
+                return;
+            }
+            playersMessage.textContent = `${currentPlayer.name} please place your ships`;
+
+            const item = document.getElementById(myShip.name);
+            item.removeAttribute('draggable');
+            item.classList.add('less-opacity');
+
+            if (currentPlayer.gameboard.horizontalPlacement) {
+                for (let i = 0; i < myShip.length; i++ ) {
+                    const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
+                    thisCell.classList.add("highlight");
+                    cellCol++;
+                }
+            } else {
+                for (let i = 0; i < myShip.length; i++ ) {
+                    const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
+                    thisCell.classList.add("highlight");
+                    cellRow++;
+                }
+            }
+            currentPlayer.setupShips++;
+            checkSetupProgress(); // Rename this passed in function?
+        });
+    });
+}
+
+export function addShipDragListeners(allShips) {
+    allShips.forEach(ship => {
+        ship.addEventListener('dragstart', (e) => {
+            setShipName(e.target.id);
+        });
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import './styles.css'
-import { Ship } from './ship.js';
-import { Gameboard } from './gameboard.js';
-import { Player } from './player.js';
+// import { Ship } from './ship.js';
+// import { Gameboard } from './gameboard.js';
+// import { Player } from './player.js';
 import * as play from './playBattleships.js';
-import * as listeners from './eventListeners.js';
+import { newGameBtn, resetBtn, toggleBtn, nxtPlayerSetupBtn, startGameBtn,
+        playersBoard, playersMessage, oppBoard, allShips } from './domElements.js';
+import { player1, player2, getCurrentPlayer, changePlayer } from './gameState.js';
+import { addCellListeners, addShipDragListeners } from './dragAndDrop.js';
 
-    const newGameBtn = document.getElementById('new-game');
-    const resetBtn = document.getElementById('reset');
-    const nxtPlayerSetupBtn = document.getElementById('nextPlayerSetup');
-    const startGameBtn = document.getElementById('startGame');
-    const nxtPlayerGameBtn = document.getElementById('nextPLayerGame');
-    const toggleBtn = document.getElementById('toggleDir');
-    const player1 = new Player("Player1");
-    const player2 = new Player("Player2");
-    const playersBoard = document.querySelector('.players-board');
-    const playersMessage = document.getElementById('players-message');
-    const oppBoard = document.querySelector('.opp-board');
-    const oppMessage = document.getElementById('opps-message');
-    const allShips = document.querySelectorAll('[draggable="true"]');
+function checkSetupProgress() {
+    const currentPlayer = getCurrentPlayer();
 
-    let playercells;
-    let currentPlayer = player1;
-    let shipName;
-    let previewCells = [];
+    if (player1.setupShips === 5 && player2.setupShips === 5) {
+        startGameBtn.classList.remove('invisible');
+    } else if (currentPlayer.setupShips === 5) {
+        playersBoard.textContent = '';
+        playersMessage.textContent = 'Pass to the next player and let them click the button below' 
+        nxtPlayerSetupBtn.classList.remove('invisible');
+        // player1.setupShips = true;
+    } else {
+        playersMessage.textContent = `${currentPlayer.name} please place your ships`;
+    }
+}
 
-
-    startNewGame();
+    // startNewGame();
     // After each drop, check if player finished setup. 
     // If finished, show next player button.
     
@@ -34,139 +32,130 @@ import * as listeners from './eventListeners.js';
 
 
 
+function startNewGame() {
+    const currentPlayer = getCurrentPlayer();
 
+    playersBoard.textContent = '';
+    playersMessage.textContent = `${currentPlayer.name} please place your ships`;
+    oppBoard.textContent = '';
+    // oppMessage.textContent = '';
+    player1.resetBoard();
+    player2.resetBoard();
 
+    play.newGame();
+    addCellListeners(checkSetupProgress);
+    play.resetDraggableShips(allShips);
 
+    document.querySelectorAll('.betweenBtn').forEach(btn => btn.classList.add('invisible'));
+}
 
+startNewGame();
+addShipDragListeners(allShips);
 
+newGameBtn.addEventListener('click', startNewGame);
 
+resetBtn.addEventListener('click', () => {
+    getCurrentPlayer().resetBoard();
+    startNewGame();
+});
 
+toggleBtn.addEventListener('click', () => {
+    const currentPlayer = getCurrentPlayer();
+    currentPlayer.gameboard.switchDirection();
+    toggleBtn.textContent = currentPlayer.gameboard.horizontalPlacement
+        ? 'Horizontal Placement'
+        : 'Vertical Placement';
+    // if (currentPlayer.gameboard.horizontalPlacement == false) {
+    //     toggleBtn.textContent = "Vertical Placement";
+    // } else {
+    //     toggleBtn.textContent = "Horizontal Placement";
+    // }
+})
 
-
-
-
-    
-
-    function startNewGame() {
-        currentPlayer = player1;
-        playersBoard.textContent = '';
-        playersMessage.textContent = `${currentPlayer.name} please place your ships`;
-        oppBoard.textContent = '';
-        oppMessage.textContent = '';
-        player1.resetBoard();
-        player2.resetBoard();
-
-        play.newGame();
-        addCellListeners();
-        play.resetDraggableShips(allShips);
-
-        let nextBtns = document.querySelectorAll('.betweenBtn');
-        nextBtns.forEach(btn => btn.classList.add('invisible'));
-    }
-
-
-
-    function changePlayer() {
-       currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
-    }
-
-    newGameBtn.addEventListener('click', startNewGame);
-
-    resetBtn.addEventListener('click', (e) => {
-        currentPlayer.resetBoard();
-        startNewGame();
-    });
-
-    toggleBtn.addEventListener('click', (e) => {
-        currentPlayer.gameboard.switchDirection();
-        if (currentPlayer.gameboard.horizontalPlacement == false) {
-            toggleBtn.textContent = "Vertical Placement";
-        } else {
-            toggleBtn.textContent = "Horizontal Placement";
-        }
-    })
+nxtPlayerSetupBtn.addEventListener('click', () => {
+    changePlayer();
+    const currentPlayer = getCurrentPlayer();
+    nxtPlayerSetupBtn.classList.add('invisible');
+    playersMessage.textContent = `${currentPlayer.name} please place your ships`;
+    play.updatePlayersGrid(currentPlayer.gameboard);
+    play.resetDraggableShips(allShips);
+    addCellListeners(checkSetupProgress);
+});
 
     
 
 
 
-    function addCellListeners() {
-        playercells = document.querySelectorAll(".grid-cell");
+    // function addCellListeners() {
+    //     playercells = document.querySelectorAll(".grid-cell");
 
-        playercells.forEach(cell => {
-        cell.addEventListener("dragover", listeners.draggingOver);
-        cell.addEventListener("dragenter", (e) => {
-            if (!shipName) return;
-            const myShip = currentPlayer.gameboard[shipName];
-            listeners.previewShipPlacement(e, myShip, currentPlayer.gameboard.horizontalPlacement, previewCells);
-        });
+    //     // playercells.forEach(cell => {
+    //     // cell.addEventListener("dragover", listeners.draggingOver);
+    //     // cell.addEventListener("dragenter", (e) => {
+    //     //     if (!shipName) return;
+    //     //     const myShip = currentPlayer.gameboard[shipName];
+    //     //     listeners.previewShipPlacement(e, myShip, currentPlayer.gameboard.horizontalPlacement, previewCells);
+    //     // });
 
-        cell.addEventListener("dragleave", listeners.removePreview);
+    //     // cell.addEventListener("dragleave", listeners.removePreview);
 
 
-        cell.addEventListener("drop", (e) => {
-            listeners.removePreview(e);
-            listeners.clearPreviewCells(previewCells);
-            let cellRow = e.target.dataset.row;
-            let cellCol = e.target.dataset.col;
-            let myShip = currentPlayer.gameboard[shipName];
+    //     // cell.addEventListener("drop", (e) => {
+    //     //     listeners.removePreview(e);
+    //     //     listeners.clearPreviewCells(previewCells);
+    //     //     let cellRow = e.target.dataset.row;
+    //     //     let cellCol = e.target.dataset.col;
+    //     //     let myShip = currentPlayer.gameboard[shipName];
 
-            try {
-                currentPlayer.gameboard.placeShip(myShip, [cellRow, cellCol]);    
-            } catch (error) {
-                playersMessage.textContent = error.message;
-                return;
-            }
-            playersMessage.textContent = `${currentPlayer.name} please place your ships`;
+    //     //     try {
+    //     //         currentPlayer.gameboard.placeShip(myShip, [cellRow, cellCol]);    
+    //     //     } catch (error) {
+    //     //         playersMessage.textContent = error.message;
+    //     //         return;
+    //     //     }
+    //         // playersMessage.textContent = `${currentPlayer.name} please place your ships`;
 
-            const item = document.getElementById(myShip.name);
-            item.removeAttribute('draggable');
-            item.classList.add('less-opacity');
+    //         // const item = document.getElementById(myShip.name);
+    //         // item.removeAttribute('draggable');
+    //         // item.classList.add('less-opacity');
             
-            if (currentPlayer.gameboard.horizontalPlacement == true) {
-                for (let i = 0; i < myShip.length; i++ ) {
-                    const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
-                    thisCell.classList.add("highlight");
-                    cellCol++;
-                }
-            } else {
-                for (let i = 0; i < myShip.length; i++ ) {
-                    const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
-                    thisCell.classList.add("highlight");
-                    cellRow++;
-                }
-            }
-            currentPlayer.setupShips++;
-            // Run check to see if setup finished
-            if ((player1.setupShips == true) && (player2.setupShips == 5)) {
-                // nxtPlayerBtn.classList.add('invisible');
-                startGameBtn.classList.remove('invisible');
-                player1.setupShips = true;
-            } else if (player1.setupShips == 5) {
-                // Make screen blank or with message
-                playersBoard.textContent = '';
-                playersMessage.textContent = 'Pass to the next player and let them click the button below' 
-                nxtPlayerSetupBtn.classList.remove('invisible');
-                player1.setupShips = true;
-            }
-        });  
-    })
-    }
+    //         // if (currentPlayer.gameboard.horizontalPlacement == true) {
+    //         //     for (let i = 0; i < myShip.length; i++ ) {
+    //         //         const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
+    //         //         thisCell.classList.add("highlight");
+    //         //         cellCol++;
+    //         //     }
+    //         // } else {
+    //         //     for (let i = 0; i < myShip.length; i++ ) {
+    //         //         const thisCell = document.querySelector(`[data-row='${cellRow}'][data-col='${cellCol}']`);
+    //         //         thisCell.classList.add("highlight");
+    //         //         cellRow++;
+    //         //     }
+    //         // }
+    //         // currentPlayer.setupShips++;
+    //         // Run check to see if setup finished
+    //         // if ((player1.setupShips == true) && (player2.setupShips == 5)) {
+    //         //     // nxtPlayerBtn.classList.add('invisible');
+    //         //     startGameBtn.classList.remove('invisible');
+    //         //     player1.setupShips = true;
+    //         // } else if (player1.setupShips == 5) {
+    //         //     // Make screen blank or with message
+    //         //     playersBoard.textContent = '';
+    //         //     playersMessage.textContent = 'Pass to the next player and let them click the button below' 
+    //         //     nxtPlayerSetupBtn.classList.remove('invisible');
+    //         //     player1.setupShips = true;
+    //         // }
+    //     });  
+    // })
+    // }
     // Need to set all the drag elements again on draggable ships
-    nxtPlayerSetupBtn.addEventListener('click', (e) => {
-        changePlayer();
-        nxtPlayerSetupBtn.classList.add('invisible');
-        playersMessage.textContent = `${currentPlayer.name} please place your ships`;
-        play.updatePlayersGrid(currentPlayer.gameboard);
-        play.resetDraggableShips(allShips);
-        addCellListeners();
-    });
 
-    allShips.forEach(ship => {
-        ship.addEventListener('dragstart', (e) => {
-            shipName = e.target.id;
-        });
-    })
+
+    // allShips.forEach(ship => {
+    //     ship.addEventListener('dragstart', (e) => {
+    //         shipName = e.target.id;
+    //     });
+    // })
 
 
   
